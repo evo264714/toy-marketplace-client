cbceff19d1aca15a52d05047991e9d500b7d925a
evo264714 <mh264714@gmail.com>
2023-05-21 22:46:53 +0600

    Sweet alert added

--- src/pages/ToyCard/ToyCard.jsx
+++ src/pages/ToyCard/ToyCard.jsx
@@ -1,11 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ToyCard = ({ toy }) => {
-    
+
     const { _id, toyName, pictureURL, price, rating } = toy;
+
+    const navigate = useNavigate()
+
+    const handleViewDetails = (id) => {
+        Swal.fire({
+            title: "Forbidden!!!",
+            text: "You Have To Login First",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Delete!",
+          }).then((result) => {
+            if (result.isConfirmed) {
+                navigate(`/toyDetails/${_id}`)
+            }
+          });
+
+    }
     
+    // 
+
 
     
+
+
+
     return (
         <div className="flex justify-center">
             <div className="card w-96 bg-black shadow-xl my-10">
@@ -14,9 +39,9 @@ const ToyCard = ({ toy }) => {
                     <h2 className="text-3xl font-bold text-center text-cyan-300">{toyName}</h2>
                     <p className="text-warning font-semibold text-xl">Price: ${price}</p>
                     <p className="text-red-300 font-semibold text-xl mb-4">Rating: {rating}</p>
-                    
+
                     <div className="card-actions flex justify-center">
-                       <Link to={`/toyDetails/${_id}`}><button className="btn btn-accent ">View Details</button></Link>
+                        <button onClick={() => handleViewDetails(_id)} className="btn btn-accent ">View Details</button>
                     </div>
                 </div>
             </div>
